64eaa4b9a175914c61352ed24c8df77179902302
mdhasan111 <84634393+mdhasan111@users.noreply.github.com>
2025-08-19 21:33:50 +0600

    Update info.py

--- info.py
+++ info.py
@@ -105,7 +105,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("FQDN", "")
+URL = environ.get("FQDN", "https://movies-usut.onrender.com")
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
